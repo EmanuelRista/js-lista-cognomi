b60192846c959afb4fb26c39779c243f42e3e007
Emanuel <e.rista@outlook.it>
2020-10-26 13:04:07 +0100

    end (ma non funziona)

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Raccolta input
+var surnames = ['Rossi', 'Verdi', 'Gialli'];
+var userSurname = prompt();
+
+
+
+// Elaborazione dati
+
+// aggiungo elemento generato dall utente all array
+surnames.push(userSurname);
+
+// trasformo tutti gli elementi dell array in stringhe lower cased
+var surnamesLowerCase = [];
+for (var i = 0; i < surnames.length; i++){
+  surnamesLowerCase.push(surnames[i].toLowerCase());
+}
+
+// ordino l'array di stringhe
+surnamesLowerCase.sort();
+
+// Stampo l'output dei dati che ho elaborato
+var i = 0;
+while( i < surnamesLowerCase.length ){
+
+  // stampa l'elemento <li> nell'HTML
+  var firstLetter = surnamesLowerCase[i].charAt(0);
+  firstLetter = firstLetter.toUpperCase();
+  var surnameWithUpperCapital = firstLetter + surnamesLowerCase[i].slice(1);
+  document.getElementById('list').innerHTML += '<li>'+surnameWithUpperCapital+'</li>';
+
+  // memorizza l'indice della stringa inserita dall'utente nell'array ordinato
+  if( surnamesLowerCase[i] == userSurname.toLowerCase() ){
+    document.getElementById('position').innerHTML = i + 1;
+  }
+
+  i++;
+}
